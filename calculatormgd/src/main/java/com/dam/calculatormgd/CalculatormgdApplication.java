df6f65860beebd5142bd755349266e0e4b47eaac
Dario Muñiz Gonzalez <124350696+Dariomg@users.noreply.github.com>
2026-04-23 19:48:32 +0200

    Project uploaded

---
+++ calculatormgd/src/main/java/com/dam/calculatormgd/CalculatormgdApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatormgd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatormgdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatormgdApplication.class, args);
+	}
+
+}
---
+++ calculatormgd/src/test/java/com/dam/calculatormgd/CalculatormgdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatormgd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatormgdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
